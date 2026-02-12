9ccfc5c0cb94b2af85fb49f3f9629ccb8d2d94d6
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2026-02-12 19:36:38 +0530

    Refactor Page3 component for better layout and styles
    Refactor Page3 component layout and styles for improved responsiveness and organization.

--- Components/Page3.js
+++ Components/Page3.js
@@ -5,10 +5,10 @@ import React from 'react'
 const Page3 = () => {
   return (
     <>
-    <div  className='min-h-[290vh] width-[100vw] overflow-hidden
-    bg-black font-[gilroy]'>
-      <div className="bg-zinc-900 shadow-lg backdrop-blur-lg min-h-screen w-[90%] rounded-2xl mx-auto flex items-center justify-center p-5">
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 ">
+    <div  className='bg-black font-[gilroy] overflow-hidden py-16'>
+      <div className="bg-zinc-900 shadow-lg backdrop-blur-lg 
+    w-full max-w-7xl mx-auto rounded-2xl p-4 sm:p-6 lg:p-10">
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
 
     <div className="h-50 p-5 rounded-2xl shadow-lg backdrop-blur-lg bg-black border border-white/20 transition transform hover:scale-105 hover:shadow-2xl duration-500">
       <h3 className="text-5xl font-extrabold text-white text-center mb-2">3+</h3>
@@ -65,78 +65,209 @@ const Page3 = () => {
 
   </div>
 </div>
-<h1 id='Page3' className='text-center font-[gilroy] uppercase font-extrabold text-5xl text-white mt-30'>HAVE A LOOK ON MY <span className='text-center font-[gilroy] uppercase font-extrabold text-5xl text-green-600'>SKILLS</span></h1>
+<h1 id='Page3' className='text-center font-[gilroy] uppercase font-extrabold text-xl lg:text-4xl text-white mt-30'>HAVE A LOOK ON MY <span className='text-center font-[gilroy] uppercase font-extrabold text-2xl lg:text-5xl text-green-600'>SKILLS</span></h1>
 {/*<Lottie 
         animationData={Wave} 
         loop={true} 
         style={{ width: 300, height: 300 }}
       />*/}
-        <hr className='w-50 text-4xl font-extrabold mt-1 text-green-800 mx-auto'></hr>
-        <h1 className='ml-9 font-[gilroy] uppercase font-extrabold text-4xl text-white mt-20 text-center'>FRONTEND</h1>
-        <div className='flex items-center justify-around gap-2 w-[90vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Javascript</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>React.js</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Next.js</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>TailwindCSS</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Bootstrap 5</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>GSAP</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Three.js</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Material UI</div>
-        </div>
-        <div className='flex items-center justify-around w-[50vw] mx-auto mt-10 text-white font-[gilroy]'>
-           <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>FIGMA</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Framer Motion</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>React Query</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347] hover:scale-105 hover:shadow-2xl duration-500'>Chakra UI</div>
-          
-        </div>
-        <h1 className='ml-9 font-[gilroy] uppercase font-extrabold text-4xl text-white mt-20 text-center'>BACKEND</h1>
-        <div className='flex items-center justify-around gap-2 w-[90vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>MySql</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>MongoDB</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>Express</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>Node.js</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>Rest API</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>GraphQL</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>Firebase</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>JWT Auth</div>
-        </div>
-        <div className='flex items-center justify-around w-[40vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>JEST</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>POSTMAN</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]'>DOCKER</div>
-        </div>
-         <h1 className='ml-9 font-[gilroy] uppercase font-extrabold text-4xl text-white mt-20 text-center'>OTHERS</h1>
-        <div className='flex items-center justify-around gap-2 w-[90vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>JAVA</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>PYTHON</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>AWS</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>GIT</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>GITHUB</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>Cloudflare</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>Vercel</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>Render</div>
-        </div>
-        <div className='flex items-center justify-around w-[40vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>VS CODE</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>IntelliJ</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66] hover:scale-105 hover:shadow-2xl duration-500'>Gen AI</div>
-        </div>
-         <h1 className='ml-9 font-[gilroy] uppercase font-extrabold text-4xl text-white mt-20 text-center'>ACADEMICS</h1>
-        <div className='flex items-center justify-around gap-2 w-[90vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>OOPS</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>DBMS</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-42 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Operating System</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-42 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Computer networks</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-35 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Data structures</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-44 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Software engineering</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-40 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>ADA</div>
-        </div>
-        <div className='flex items-center justify-around w-[45vw] mx-auto mt-10 text-white font-[gilroy]'>
-          <div className='border-[1px] border-white/40 rounded-2xl w-44 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Problem solving</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-46 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Communication skills</div>
-          <div className='border-[1px] border-white/40 rounded-2xl w-46 p-1 text-center text-md shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff] hover:scale-105 hover:shadow-2xl duration-500'>Project management</div>
-        </div>
+        <hr className='w-24 lg:w-48 h-1 border-none bg-green-800 mx-auto mt-2'></hr>
+        <h1 className="font-[gilroy] uppercase font-extrabold text-2xl sm:text-3xl lg:text-4xl text-white mt-20 text-center">
+  FRONTEND
+</h1>
+
+<div
+  className="
+    flex flex-wrap 
+    justify-center 
+    gap-5 sm:gap-4 lg:gap-10
+    max-w-6xl 
+    mx-auto 
+    mt-8 
+    px-4
+    text-white 
+    font-[gilroy]
+  "
+>
+  {[
+    "Javascript",
+    "React.js",
+    "Next.js",
+    "TailwindCSS",
+    "Bootstrap 5",
+    "GSAP",
+    "Three.js",
+    "Material UI",
+    "Figma",
+    "Framer Motion",
+    "React Query",
+    "Chakra UI",
+  ].map((skill, index) => (
+    <div
+      key={index}
+      className="
+        border border-white/40
+        rounded-2xl
+        px-6 py-1.5
+        text-sm sm:text-base
+        text-center
+        whitespace-nowrap
+        shadow-[0_0_12px_#ff9900,0_0_20px_#ffb347]
+        transition-transform duration-300
+        hover:scale-105 hover:shadow-2xl
+      "
+    >
+      {skill}
+    </div>
+  ))}
+</div>
+
+        <h1 className="font-[gilroy] uppercase font-extrabold text-2xl sm:text-3xl lg:text-4xl text-white mt-16 text-center">
+  BACKEND
+</h1>
+
+<div
+  className="
+    flex flex-wrap
+    justify-center
+    gap-5 sm:gap-4 lg:gap-10
+    max-w-6xl
+    mx-auto
+    mt-8
+    px-4
+    text-white
+    font-[gilroy]
+  "
+>
+  {[
+    "MySQL",
+    "MongoDB",
+    "Express",
+    "Node.js",
+    "REST API",
+    "GraphQL",
+    "Firebase",
+    "JWT Auth",
+    "Jest",
+    "Postman",
+    "Docker",
+  ].map((skill, index) => (
+    <div
+      key={index}
+      className="
+        border border-white/40
+        rounded-2xl
+        px-6 py-1.5
+        text-sm sm:text-base
+        text-center
+        whitespace-nowrap
+        shadow-[0_0_12px_#ff4d4d,0_0_20px_#ff9999]
+        transition-transform duration-300
+        hover:scale-105 hover:shadow-2xl
+      "
+    >
+      {skill}
+    </div>
+  ))}
+</div>
+
+         <h1 className="font-[gilroy] uppercase font-extrabold text-2xl sm:text-3xl lg:text-4xl text-white mt-16 text-center">
+  TOOLS & OTHERS
+</h1>
+
+<div
+  className="
+    flex flex-wrap
+    justify-center
+    gap-5 sm:gap-4 lg:gap-10
+    max-w-6xl
+    mx-auto
+    mt-8
+    px-4
+    text-white
+    font-[gilroy]
+  "
+>
+  {[
+    "Java",
+    "Python",
+    "AWS",
+    "Git",
+    "GitHub",
+    "Cloudflare",
+    "Vercel",
+    "Render",
+    "VS Code",
+    "IntelliJ",
+    "Gen AI",
+  ].map((skill, index) => (
+    <div
+      key={index}
+      className="
+        border border-white/40
+        rounded-2xl
+        px-6 py-1.5
+        text-sm sm:text-base
+        text-center
+        whitespace-nowrap
+        shadow-[0_0_12px_#00ff00,0_0_20px_#66ff66]
+        transition-transform duration-300
+        hover:scale-105 hover:shadow-2xl
+      "
+    >
+      {skill}
+    </div>
+  ))}
+</div>
+
+        <h1 className="font-[gilroy] uppercase font-extrabold text-2xl sm:text-3xl lg:text-4xl text-white mt-16 text-center">
+  ACADEMICS
+</h1>
+
+<div
+  className="
+    flex flex-wrap
+    justify-center
+    gap-5 sm:gap-4 lg:gap-10
+    max-w-6xl
+    mx-auto
+    mt-8
+    px-4
+    text-white
+    font-[gilroy]
+  "
+>
+  {[
+    "OOPS",
+    "DBMS",
+    "Operating Systems",
+    "Computer Networks",
+    "Data Structures",
+    "Software Engineering",
+    "ADA",
+    "Problem Solving",
+    "Communication Skills",
+    "Project Management",
+  ].map((skill, index) => (
+    <div
+      key={index}
+      className="
+        border border-white/40
+        rounded-2xl
+        px-6 py-1.5
+        text-sm sm:text-base
+        text-center
+        whitespace-nowrap
+        shadow-[0_0_12px_#00ffff,0_0_20px_#66ffff]
+        transition-transform duration-300
+        hover:scale-105 hover:shadow-2xl
+      "
+    >
+      {skill}
+    </div>
+  ))}
+</div>
+
     </div>
     </>
   )
