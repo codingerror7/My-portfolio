5d92de83ad8135a5b59a81aacb8e65506f555d02
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2026-02-12 19:58:16 +0530

    Update Page6.js

--- Components/Page6.js
+++ Components/Page6.js
@@ -97,7 +97,7 @@ const Page6 = () => {
   rounded-xl
   backdrop-blur-md bg-black/40
   shadow-[0_0_18px_rgba(255,0,0,0.6)]'>
-    <p class="text-base sm:text-lg text-center">2024 - Microsoft azure Fundamnetals Certification</p></div>
+    <p className="text-base sm:text-lg text-center">2024 - Microsoft azure Fundamnetals Certification</p></div>
     </div>
     <p className="text-base sm:text-xl text-center mt-6">• Solved 500+ Coding questions on Codechef.</p>
     <div className='w-full sm:w-[80%] md:w-[60%]
@@ -125,3 +125,4 @@ const Page6 = () => {
 }
 
 export default Page6
+
