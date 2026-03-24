332812edfe79290ce93239ef8408761c8c6dff2b
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2026-03-24 13:42:02 +0530

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
 
     {/* Resume Button */}
     <a
-      href="/Sujal_resume(1).pdf"
+      href="/Sujal_resume (1).pdf"
       download
       className="flex items-center gap-2 px-2 py-2 lg:px-5 lg:py-2 mt-[-12] lg:mt-0
           rounded-full text-black font-semibold text-sm sm:text-base
